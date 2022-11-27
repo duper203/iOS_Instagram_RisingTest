66f29d748759f6abe3e67424cf325a0ec1e848b2
duper <hsookim203@gmail.com>
2022-11-27 17:45:33 +0900

    로그인 회원가입

---
+++ instagram/Login.swift
@@ -0,0 +1,22 @@
+//
+//  Login.swift
+//  instagram
+//
+//  Created by 김혜수 on 2022/11/27.
+//
+
+import Foundation
+import UIKit
+
+class LoginVC: UIViewController{
+    
+    
+    @IBAction func registerBtn(_ sender: Any) {
+        let pushVC = self.storyboard?.instantiateViewController(withIdentifier: "Register")
+                self.navigationController?.pushViewController(pushVC!, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
