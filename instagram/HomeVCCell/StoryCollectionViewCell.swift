3b35584b3a09d857bd727875b8b652a4731f2428
duper <hsookim203@gmail.com>
2022-11-28 18:13:41 +0900

    홈화면 구현 완료 1

--- instagram/HomeVCCell/StoryCollectionViewCell.swift
+++ instagram/HomeVCCell/StoryCollectionViewCell.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class StoryCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var profileBgOne: UIView!
+    @IBOutlet weak var profileBgTwo: UIView!
+    @IBOutlet weak var profileImg: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        profileBgOne.layer.cornerRadius = 24
+        profileBgTwo.layer.cornerRadius = 23.5
+        profileImg.layer.cornerRadius = 22.5
+        profileImg.clipsToBounds = true
     }
 
 }
